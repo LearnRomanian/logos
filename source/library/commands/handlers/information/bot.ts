9a654eec8d8ea78be0f82903ccc6b3b31b62f72a
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-08-09 12:28:30 +0100

    feat: Migrate the `/information bot` command to components v2.

--- source/library/commands/handlers/information/bot.ts
+++ source/library/commands/handlers/information/bot.ts
@@ -21,41 +21,44 @@ async function handleDisplayBotInformation(client: Client, interaction: Logos.In
 		.notice(
 			interaction,
 			{
-				embeds: [
+				flags: Discord.MessageFlags.IsComponentV2,
+				components: [
 					{
-						author: {
-							iconUrl: Discord.avatarUrl(client.bot.id, botUser.discriminator, {
-								avatar: botUser.avatar ?? undefined,
-								format: "png",
-							}),
-							name: botUser.username,
-						},
-						fields: [
+						type: Discord.MessageComponentTypes.Container,
+						components: [
 							{
-								name: `${constants.emojis.commands.information.bot.features.bot} ${strings.concept.title}`,
-								value: strings.concept.description,
+								type: Discord.MessageComponentTypes.TextDisplay,
+								content: `### ${constants.emojis.commands.information.bot.features.bot} ${strings.concept.title}\n${strings.concept.description}`,
 							},
 							{
-								name: `${constants.emojis.commands.information.bot.features.function} ${strings.function.title}`,
-								value: `${strings.function.description}\n${featuresFormatted}`,
+								type: Discord.MessageComponentTypes.TextDisplay,
+								content: `### ${constants.emojis.commands.information.bot.features.function} ${strings.function.title}\n${strings.function.description}\n${featuresFormatted}`,
 							},
 							{
-								name: `${constants.emojis.commands.information.bot.features.languages} ${strings.languages.title}`,
-								value: strings.languages.description,
+								type: Discord.MessageComponentTypes.TextDisplay,
+								content: `### ${constants.emojis.commands.information.bot.features.languages} ${strings.languages.title}\n${strings.languages.description}`,
 							},
+							...(interaction.parameters.show
+								? []
+								: ([
+										{
+											type: Discord.MessageComponentTypes.Separator,
+											spacing: Discord.SeparatorSpacingSize.Large,
+										},
+										{
+											type: Discord.MessageComponentTypes.ActionRow,
+											components: interaction.parameters.show
+												? []
+												: [
+														client.services
+															.global("interactionRepetition")
+															.getShowButton(interaction),
+													],
+										},
+									] satisfies Discord.ContainerComponent["components"])),
 						],
 					},
 				],
-				components: interaction.parameters.show
-					? []
-					: [
-							{
-								type: Discord.MessageComponentTypes.ActionRow,
-								components: [
-									client.services.global("interactionRepetition").getShowButton(interaction),
-								],
-							},
-						],
 			},
 			{ visible: interaction.parameters.show },
 		)
