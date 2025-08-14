dbdd86dc73057bcac0d4dadd9f344e97c596ea78
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-08-14 16:42:49 +0100

    feat: Migrate acknowledgements to components v2.

--- source/library/commands/handlers/acknowledgements.ts
+++ source/library/commands/handlers/acknowledgements.ts
@@ -1,18 +1,29 @@
 import type { Client } from "logos/client";
 
 async function handleDisplayAcknowledgements(client: Client, interaction: Logos.Interaction): Promise<void> {
-	const fields = constants.acknowledgements.map<Discord.Camelize<Discord.DiscordEmbedField>>((acknowledgement) => {
+	const fields = constants.acknowledgements.map((acknowledgement) => {
 		const contributorsFormatted = acknowledgement.users.map((contributor) => contributor.username).join(", ");
 
-		return {
-			name: `${contributorsFormatted}:`,
-			value: acknowledgement.reason,
-			inline: false,
-		};
+		return `### ${contributorsFormatted}:\n${acknowledgement.reason}`;
 	});
 
 	const strings = constants.contexts.acknowledgements({ localise: client.localise, locale: interaction.locale });
-	client.notice(interaction, { title: strings.acknowledgements, fields }).ignore();
+	client
+		.notice(interaction, {
+			flags: Discord.MessageFlags.IsComponentV2,
+			components: [
+				{
+					type: Discord.MessageComponentTypes.Container,
+					components: [
+						{
+							type: Discord.MessageComponentTypes.TextDisplay,
+							content: `# ${strings.acknowledgements}\n${fields.join("\n")}`,
+						},
+					],
+				},
+			],
+		})
+		.ignore();
 }
 
 export { handleDisplayAcknowledgements };
